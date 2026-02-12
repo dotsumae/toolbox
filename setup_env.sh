8fb1cd0b5456d699732a9f63e0c216a6c79a5cfa
Alex Salazar <33135381+dotsumae@users.noreply.github.com>
2026-02-12 17:02:40 +0100

    Enhance setup_env.sh with alias and config updates
    Updated user configuration and added alias setup for convenience.

--- setup_env.sh
+++ setup_env.sh
@@ -4,21 +4,32 @@ echo "Installing tools..."
 sudo apt update && sudo apt upgrade -y
 sudo apt install trash-cli vim sl zsh pipx autojump wget nala fzf -y
 sudo apt install tldr -y || pipx install tldr
+tldr -u
 
-echo "Updating user config..."
+echo "Installing pentest-related tools if using Kali"
+sudo apt install seclists peass autorecon zaproxy
+
+
+echo "Updating core user config..."
 cd ~
 mv .zshrc .zshrc.bak
 wget -O .zshrc https://git.grml.org/f/grml-etc-core/etc/zsh/zshrc
 echo "# Load autojump" >> .zshrc
 echo ". /usr/share/autojump/autojump.sh" >> .zshrc
 echo "export IP=$(curl ifconfig.me) >> .zshrc
-echo "source ~/.alias" >> .zshrc
-echo "alias rm='trash'" >> .alias
-echo "alias update-all='sudo nala upgrade --download-only -y ; sudo apt update && sudo apt upgrade -y'" >> .alias
 mkdir -p ~/Executables/bin/
 echo 'PATH="$USER/Executables/bin:$PATH"' >> .zshrc
+
+
+echo "Preparing aliases"
+echo "source ~/.alias" >> .zshrc
+echo "alias rm='trash'" >> .alias
+echo "alias update-all='sudo apt upgrade --download-only -y ; sudo apt update && sudo apt upgrade -y'" >> .alias
+
 
 echo "Changing shell to zsh..."
 chsh -s /bin/zsh
 #echo "zsh" >> .bashrc
 echo "All OK!"
+
+echo "Installing nice dotfiles"
