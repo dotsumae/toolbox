9b8009c2c48342a0c298cba4393f7fcf0507cb14
Alex <contact@salazaralex.com>
2026-02-15 13:54:15 +0100

    started working on better format

--- setup_env.sh
+++ setup_env.sh
@@ -2,35 +2,31 @@
 
 echo "Installing tools..."
 sudo apt update && sudo apt upgrade -y
-sudo apt install trash-cli vim sl zsh pipx autojump wget nala fzf -y
+sudo apt install trash-cli vim sl zsh pipx autojump wget nala fzf sd -y
 sudo apt install tldr -y || pipx install tldr
 tldr -u
 
 echo "Installing pentest-related tools if using Kali"
 sudo apt install seclists peass autorecon zaproxy
 
+echo "Deploying dotfiles..."
+mkdir -p $HOME/Executables/bin/
+
+cp .alias $HOME/
+cp start-pentest $HOME/Executables/bin/
 
 echo "Updating core user config..."
-cd ~
+cd $HOME
 mv .zshrc .zshrc.bak
 wget -O .zshrc https://git.grml.org/f/grml-etc-core/etc/zsh/zshrc
+
 echo "# Load autojump" >> .zshrc
 echo ". /usr/share/autojump/autojump.sh" >> .zshrc
-echo "export IP=$(curl ifconfig.me) >> .zshrc
-mkdir -p ~/Executables/bin/
 echo 'PATH="$USER/Executables/bin:$PATH"' >> .zshrc
-
-
-echo "Preparing aliases"
 echo "source ~/.alias" >> .zshrc
-echo "alias rm='trash'" >> .alias
-echo "alias inc-nmap='nmap -A -iL targets.txt -oN nmap.txt && nmap -A -sS -sU -p 0-65535 -iL targets.txt -oN nmap_deeper.txt'" >> .alias
-echo "alias update-all='sudo apt upgrade --download-only -y ; sudo apt update && sudo apt upgrade -y'" >> .alias
-
 
 echo "Changing shell to zsh..."
 chsh -s /bin/zsh
-#echo "zsh" >> .bashrc
+
 echo "All OK!"
 
-echo "Installing nice dotfiles"
