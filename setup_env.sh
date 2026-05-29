47203f83343c43f3ec10163c11ab7475cdcb7757
Alex Salazar <33135381+dotsumae@users.noreply.github.com>
2026-05-29 11:02:44 +0200

    added kali repo capability

--- setup_env.sh
+++ setup_env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Working with Kali [Y/n] ?"
 read -n 1 ans
-if [[ $ans == "n"]]; then
+if [[ $ans == "n" ]]; then
     echo "Getting the alternative mirror"
     echo "deb https://mirror.netcologne.de/kali kali-last-snapshot main contrib non-free non-free-firmware" | sudo tee -a /etc/apt/sources.list
 fi
